0de29f410bdaf2bd3123e7a962ec2d4c860a8811
Amir Tosson <45110899+amirtosson@users.noreply.github.com>
2021-04-15 08:27:15 +0200

    GUI and APIs

--- SiegMainWindow.py
+++ SiegMainWindow.py
@@ -11,25 +11,28 @@ import SiegSimulationControls as _siegSim
 
 
 class SiegMainWindow(QtWidgets.QMainWindow):
+    g = 0
+    sampleIsReady = False
+    detectorIsReady = False
+    simIsReady = False
+    rndMatrix = [0,0,0,0]
+    rndParasAreOK = False
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        #self.initUI()
-        uic.loadUi("siegmainwindow.ui", self)
-        self.fullscreenAction.triggered.connect(self.FullscreenAction)
+        self.initUI()
 
     def initUI(self):
-        self = uic.loadUi("siegmainwindow.ui")
-        self.show()
-        # QToolTip.setFont(QFont('SansSerif', 10))
-        # self.setToolTip('This is a <b>QWidget</b> widget')
-        # btn = QPushButton('Button', self)
-        # btn.clicked.connect(self.speaking_method)
-        # btn.setToolTip('This is a <b>QPushButton</b> widget')
-        # btn.resize(btn.sizeHint())
-        # btn.move(50, 50)
-        # self.setGeometry(300, 300, 300, 200)
-        # self.setWindowTitle('Tooltips')
-        # #self.show()
+        uic.loadUi("siegmainwindow.ui", self)
+        self.fullscreenAction.triggered.connect(self.FullscreenAction)
+        self.randGroupBox.setHidden(True)
+        self.configSampleButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogCancelButton')))
+        self.configDetectorButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogCancelButton')))
+        self.configSimButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogCancelButton')))
+        self.autoRandCheckBox.stateChanged.connect(self.UseAutomaticRandomization)
+        self.configSampleButton.clicked.connect(self.OpenSamplePage)
+        self.configDetectorButton.clicked.connect(self.OpenDetectorPage)
+        self.configSimButton.clicked.connect(self.OpenSimPage)
+        self.randomParaOKButton.clicked.connect(self.SubmitRandomizationPara)
         
     def speaking_method(self):                                              
         _siegSim.Test()
@@ -39,3 +42,50 @@ class SiegMainWindow(QtWidgets.QMainWindow):
             self.showNormal()
         else:
             self.showFullScreen()
+            
+    def UseAutomaticRandomization(self):
+        if self.autoRandCheckBox.isChecked():
+            self.randGroupBox.setHidden(False)
+        else:
+           self.randGroupBox.setHidden(True)
+    
+    def OpenSamplePage(self):                                              
+        self.tabWidget.setCurrentIndex(0)
+    
+    def OpenDetectorPage(self):                                              
+        self.tabWidget.setCurrentIndex(1)    
+    
+    def OpenSimPage(self):                                              
+        self.tabWidget.setCurrentIndex(2)
+        
+    def UpdateTODOButtonsIcons(self):
+        if self.sampleIsReady:
+            self.configSampleButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogApplyButton')))
+        else:
+            self.configSampleButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogCancelButton')))
+
+        if self.detectorIsReady:
+            self.configDetectorButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogApplyButton')))
+        else:
+            self.configDetectorButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogCancelButton')))
+  
+        if self.simIsReady:      
+            self.configSimButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogApplyButton')))
+        else:                         
+            self.configSimButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogCancelButton')))
+            
+    def SubmitRandomizationPara(self):
+        self.rndMatrix[0] = self.disSpinBoxMin.value()
+        self.rndMatrix[1] = self.disSpinBoxMax.value()
+        self.rndMatrix[2] = self.thickSpinBoxMin.value()
+        self.rndMatrix[3] = self.thickSpinBoxMax.value()
+        self.rndParasAreOK = self.CheckRndParas(self.rndMatrix)
+        if not self.rndParasAreOK:
+            self.errorText.setText("Enteries are not valid")
+        else:
+            self.errorText.setText("")
+    def CheckRndParas(self,rndArr):   
+        if rndArr[1] >= rndArr[0]:
+            return True
+        else:
+            return False
--- SiegSimulationControls.py
+++ SiegSimulationControls.py
@@ -21,207 +21,211 @@ t1_start=0
 t1_stop=0
 
 
-resultAll = []
 
-def InitBeam():
-    direction = ba.Direction(0.64*deg, 0.0*deg)
-    beam = ba.Beam(1e+16*0.3/80, 0.14073*nm, direction) 
-    return beam
-
-def InitDetector():
-    detector = ba.RectangularDetector(1024, 15, 1024, 51.2)
-    detector.setResolutionFunction(ba.ResolutionFunction2DGaussian(0.02, 0.02))
-    detector.setPerpendicularToReflectedBeam(1277.0, 0.75, 20.65)
-    return detector
-
-def InitSim(_sample, _beam, _detector):
-    sim = ba.GISASSimulation(_beam, _sample, _detector)
-    sim.getOptions().setUseAvgMaterials(True)
-    sim.getOptions().setIncludeSpecular(False)
-    background = ba.ConstantBackground(5e+01)
-    sim.setBackground(background)
-    return sim
-
-def InitSample():
-    t1_start = process_time.default_timer()
-    #print("starting")
-    dis1=(1+0.2*randint(0,10))*1e-05
-    dis2=(2+0.2*randint(0,10))*1e-05
-    dis3=(1+0.2*randint(0,10))*1e-05
-    dis4=(2+0.2*randint(0,10))*1e-05
-    dis5=(1+0.2*randint(0,10))*1e-05
-    dis6=(2+0.2*randint(0,10))*1e-05
-    dis7=(1+0.2*randint(0,10))*1e-05
-    dis8=(2+0.2*randint(0,10))*1e-05
-    dis9=(1+0.2*randint(0,10))*1e-05
-    dis10=(2+0.2*randint(0,10))*1e-05
-    dis11=(1+0.2*randint(0,10))*1e-05
-    dis12=(2+0.2*randint(0,10))*1e-05
-    thi1=(1+0.6*randint(0,10))
-    thi2=(1+0.6*randint(0,10))
-    thi3=(6+0.6*randint(0,10))
-    thi4=(1+0.4*randint(0,10))
-    thi5=(6+0.6*randint(0,10))
-    thi6=(1+0.4*randint(0,10))
-    thi7=(6+0.6*randint(0,10))
-    thi8=(1+0.4*randint(0,10))
-    thi9=(6+0.6*randint(0,10))
-    thi10=(1+0.4*randint(0,10))
-    thi11=(6+0.6*randint(0,10))
-    thi12=(1+0.4*randint(0,10))
-    #print("a")
-    material_1 = ba.HomogeneousMaterial("Air", 0.0, 0.0)
-    material_2 = ba.HomogeneousMaterial("TaO", dis1, 3.3e-7)
-    material_3 = ba.HomogeneousMaterial("Ta1", dis2, 2.34e-06)
-    material_4 = ba.HomogeneousMaterial("Cu3N1",dis3, 3.65e-07)
-    material_5 = ba.HomogeneousMaterial("Ta2", dis4, 2.34e-06)
-    material_6 = ba.HomogeneousMaterial("Cu3N2", dis5, 3.65e-07)
-    material_7 = ba.HomogeneousMaterial("Ta3", dis6, 2.34e-06)
-    material_8 = ba.HomogeneousMaterial("Cu3N3", dis7, 3.65e-07)
-    material_9 = ba.HomogeneousMaterial("Ta4", dis8, 2.34e-06)
-    material_10 = ba.HomogeneousMaterial("Cu3N4", dis9, 3.65e-07)
-    material_11 = ba.HomogeneousMaterial("Ta5", dis10, 2.34e-06)
-    material_12 = ba.HomogeneousMaterial("Cu3N5", dis11, 3.65e-07)
-    material_13 = ba.HomogeneousMaterial("Ta6", dis12, 2.34e-06)
-    material_14 = ba.HomogeneousMaterial("SiO2", 5.93e-06, 7.42e-08)
-    material_15 = ba.HomogeneousMaterial("Substrate", 6.31e-06, 1.21e-07)
-
-    #print("b")
-    # Defining Layers
-    layer_1 = ba.Layer(material_1)
-    layer_2 = ba.Layer(material_2,thi1)
-    layer_3 = ba.Layer(material_3,thi2)
-    layer_4 = ba.Layer(material_4, thi3)
-    layer_5 = ba.Layer(material_5, thi4)
-    layer_6 = ba.Layer(material_6, thi5)
-    layer_7 = ba.Layer(material_7, thi6)
-    layer_8 = ba.Layer(material_8, thi7)
-    layer_9 = ba.Layer(material_9, thi8)
-    layer_10 = ba.Layer(material_10, thi9)
-    layer_11 = ba.Layer(material_11, thi10)
-    layer_12 = ba.Layer(material_12, thi11)
-    layer_13 = ba.Layer(material_13, thi12)
-    layer_14 = ba.Layer(material_14, 100)
-    layer_15 = ba.Layer(material_15)
-
-    #print("c")
-    # Defining Roughness Parameters
-    layerRoughness_1 = ba.LayerRoughness(0.46,0.6, 30.0*nm)
-    layerRoughness_2 = ba.LayerRoughness(0.46, 0.6, 30.0*nm)
-    layerRoughness_3 = ba.LayerRoughness(0.39, 0.6, 30.0*nm)
-    layerRoughness_4 = ba.LayerRoughness(0.57, 0.6, 30.0*nm)
-    layerRoughness_5 = ba.LayerRoughness(0.39, 0.6, 30.0*nm)
-    layerRoughness_6 = ba.LayerRoughness(0.57, 0.6, 30.0*nm)
-    layerRoughness_7 = ba.LayerRoughness(0.39, 0.6, 30.0*nm)
-    layerRoughness_8 = ba.LayerRoughness(0.57, 0.6, 30.0*nm)
-    layerRoughness_9 = ba.LayerRoughness(0.39, 0.6, 30.0*nm)
-    layerRoughness_10 = ba.LayerRoughness(0.57, 0.6, 30.0*nm)
-    layerRoughness_11 = ba.LayerRoughness(0.39, 0.6, 30.0*nm)
-    layerRoughness_12 = ba.LayerRoughness(0.57, 0.6, 30.0*nm)
-    layerRoughness_13 = ba.LayerRoughness(0.54, 0.6, 30.0*nm)
-    layerRoughness_14 = ba.LayerRoughness(0.54, 0.6, 30.0*nm)
-    #print("d")
-    multiLayer_1 = ba.MultiLayer()
-    multiLayer_1.setCrossCorrLength(2000)
-    multiLayer_1.addLayer(layer_1)
-    multiLayer_1.addLayerWithTopRoughness(layer_2, layerRoughness_1)
-    multiLayer_1.addLayerWithTopRoughness(layer_3, layerRoughness_2)
-    multiLayer_1.addLayerWithTopRoughness(layer_4, layerRoughness_3)
-    multiLayer_1.addLayerWithTopRoughness(layer_5, layerRoughness_4)
-    multiLayer_1.addLayerWithTopRoughness(layer_6, layerRoughness_5)
-    multiLayer_1.addLayerWithTopRoughness(layer_7, layerRoughness_6)
-    multiLayer_1.addLayerWithTopRoughness(layer_8, layerRoughness_7)
-    multiLayer_1.addLayerWithTopRoughness(layer_9, layerRoughness_8)
-    multiLayer_1.addLayerWithTopRoughness(layer_10, layerRoughness_9)
-    multiLayer_1.addLayerWithTopRoughness(layer_11, layerRoughness_10)
-    multiLayer_1.addLayerWithTopRoughness(layer_12, layerRoughness_11)
-    multiLayer_1.addLayerWithTopRoughness(layer_13, layerRoughness_12)
-    multiLayer_1.addLayerWithTopRoughness(layer_14, layerRoughness_13)
-    multiLayer_1.addLayerWithTopRoughness(layer_15, layerRoughness_14)
-    t1_stop = process_time.default_timer()
-    #print("Sample_ " ,t1_stop-t1_start)
-    return multiLayer_1
-
-
-def RunSim(_simulation):
-    import ba_plot
-    #t1_start = process_time.default_timer()
-    #_simulation.getOptions().setNumberOfBatches(10)
-    #_simulation.getOptions().setNumberOfThreads(6)  
-    #print(_simulation.numberOfSimulationElements())  
-    _simulation.getOptions().setUseAvgMaterials(True)
-    _simulation.getOptions().setIncludeSpecular(False)
-    #_simulation.getOptions().setMonteCarloIntegration(False)
-
-    background = ba.ConstantBackground(5e+01)
-    _simulation.setBackground(background)
-    ba_plot.run_and_plot(_simulation)
-    #_simulation.runSimulation()  
-    #result = _simulation.result()   
-    #hist = result.histogram2d(ba.Axes.QSPACE)
-    #resultarr = hist.array()
-    #resultAll.append(resultarr)
-    #t1_stop = process_time.default_timer()
-    #print("Sim_ " ,t1_stop-t1_start)
-    #return result, hist, resultarr
-
-
-
-def StartSim(thrId):
-    #print("\n ThreadStart " ,thrId)
-    RunSim(InitSim(InitSample(), InitBeam(), InitDetector()))
-    #print("\n ThreadEnd " ,thrId)
-    return ("DONE " ,thrId)
-    #fig = plt.figure(figsize=(6, 3.2))
+class SiegSimulationControls(object):
+    resultAll = []
+    def __init__(self):
+        super().__init__
     
-   # ax = fig.add_subplot()
-   # ax.set_title('colorMap')
-    #plt.imshow(resArr)
-    
-
-
-def Test():
-    # print(threading.activeCount())
-    # t1_start = process_time.default_timer()
-    # with concurrent.futures.ProcessPoolExecutor() as executer:
-    #     results = [executer.submit(StartSim,x) for x in range(10)]
-    # for f in concurrent.futures.as_completed(results):
-    #     print(f.result())
-    
-    
-    
-    
-    #threads = list()
-    #main_thread = threading.currentThread()
-    # for index in range(threading.activeCount()):
-    #     x = threading.Thread(target=StartSim, args=(index,))
-    #     threads.append(x)
-    #     #x.start()
-    # for j in threads:
-    #     j.start()
-
-    # # Ensure all of the processes have finished
-    # for j in threads:
-    #     j.join()    
+    def InitBeam():
+        direction = ba.Direction(0.64*deg, 0.0*deg)
+        beam = ba.Beam(1e+16*0.3/80, 0.14073*nm, direction) 
+        return beam
         
-    t1_stop = process_time.default_timer()
-    print("\n ThreadingT " ,t1_stop-t1_start)
-    TestNormal()
+    def InitDetector():
+        detector = ba.RectangularDetector(1024, 15, 1024, 51.2)
+        detector.setResolutionFunction(ba.ResolutionFunction2DGaussian(0.02, 0.02))
+        detector.setPerpendicularToReflectedBeam(1277.0, 0.75, 20.65)
+        return detector
+    
+    def InitSim(_sample, _beam, _detector):
+        sim = ba.GISASSimulation(_beam, _sample, _detector)
+        sim.getOptions().setUseAvgMaterials(True)
+        sim.getOptions().setIncludeSpecular(False)
+        background = ba.ConstantBackground(5e+01)
+        sim.setBackground(background)
+        return sim
+    
+    def InitSample():
+        t1_start = process_time.default_timer()
+        #print("starting")
+        dis1=(1+0.2*randint(0,10))*1e-05
+        dis2=(2+0.2*randint(0,10))*1e-05
+        dis3=(1+0.2*randint(0,10))*1e-05
+        dis4=(2+0.2*randint(0,10))*1e-05
+        dis5=(1+0.2*randint(0,10))*1e-05
+        dis6=(2+0.2*randint(0,10))*1e-05
+        dis7=(1+0.2*randint(0,10))*1e-05
+        dis8=(2+0.2*randint(0,10))*1e-05
+        dis9=(1+0.2*randint(0,10))*1e-05
+        dis10=(2+0.2*randint(0,10))*1e-05
+        dis11=(1+0.2*randint(0,10))*1e-05
+        dis12=(2+0.2*randint(0,10))*1e-05
+        thi1=(1+0.6*randint(0,10))
+        thi2=(1+0.6*randint(0,10))
+        thi3=(6+0.6*randint(0,10))
+        thi4=(1+0.4*randint(0,10))
+        thi5=(6+0.6*randint(0,10))
+        thi6=(1+0.4*randint(0,10))
+        thi7=(6+0.6*randint(0,10))
+        thi8=(1+0.4*randint(0,10))
+        thi9=(6+0.6*randint(0,10))
+        thi10=(1+0.4*randint(0,10))
+        thi11=(6+0.6*randint(0,10))
+        thi12=(1+0.4*randint(0,10))
+        #print("a")
+        material_1 = ba.HomogeneousMaterial("Air", 0.0, 0.0)
+        material_2 = ba.HomogeneousMaterial("TaO", dis1, 3.3e-7)
+        material_3 = ba.HomogeneousMaterial("Ta1", dis2, 2.34e-06)
+        material_4 = ba.HomogeneousMaterial("Cu3N1",dis3, 3.65e-07)
+        material_5 = ba.HomogeneousMaterial("Ta2", dis4, 2.34e-06)
+        material_6 = ba.HomogeneousMaterial("Cu3N2", dis5, 3.65e-07)
+        material_7 = ba.HomogeneousMaterial("Ta3", dis6, 2.34e-06)
+        material_8 = ba.HomogeneousMaterial("Cu3N3", dis7, 3.65e-07)
+        material_9 = ba.HomogeneousMaterial("Ta4", dis8, 2.34e-06)
+        material_10 = ba.HomogeneousMaterial("Cu3N4", dis9, 3.65e-07)
+        material_11 = ba.HomogeneousMaterial("Ta5", dis10, 2.34e-06)
+        material_12 = ba.HomogeneousMaterial("Cu3N5", dis11, 3.65e-07)
+        material_13 = ba.HomogeneousMaterial("Ta6", dis12, 2.34e-06)
+        material_14 = ba.HomogeneousMaterial("SiO2", 5.93e-06, 7.42e-08)
+        material_15 = ba.HomogeneousMaterial("Substrate", 6.31e-06, 1.21e-07)
+    
+        #print("b")
+        # Defining Layers
+        layer_1 = ba.Layer(material_1)
+        layer_2 = ba.Layer(material_2,thi1)
+        layer_3 = ba.Layer(material_3,thi2)
+        layer_4 = ba.Layer(material_4, thi3)
+        layer_5 = ba.Layer(material_5, thi4)
+        layer_6 = ba.Layer(material_6, thi5)
+        layer_7 = ba.Layer(material_7, thi6)
+        layer_8 = ba.Layer(material_8, thi7)
+        layer_9 = ba.Layer(material_9, thi8)
+        layer_10 = ba.Layer(material_10, thi9)
+        layer_11 = ba.Layer(material_11, thi10)
+        layer_12 = ba.Layer(material_12, thi11)
+        layer_13 = ba.Layer(material_13, thi12)
+        layer_14 = ba.Layer(material_14, 100)
+        layer_15 = ba.Layer(material_15)
+    
+        #print("c")
+        # Defining Roughness Parameters
+        layerRoughness_1 = ba.LayerRoughness(0.46,0.6, 30.0*nm)
+        layerRoughness_2 = ba.LayerRoughness(0.46, 0.6, 30.0*nm)
+        layerRoughness_3 = ba.LayerRoughness(0.39, 0.6, 30.0*nm)
+        layerRoughness_4 = ba.LayerRoughness(0.57, 0.6, 30.0*nm)
+        layerRoughness_5 = ba.LayerRoughness(0.39, 0.6, 30.0*nm)
+        layerRoughness_6 = ba.LayerRoughness(0.57, 0.6, 30.0*nm)
+        layerRoughness_7 = ba.LayerRoughness(0.39, 0.6, 30.0*nm)
+        layerRoughness_8 = ba.LayerRoughness(0.57, 0.6, 30.0*nm)
+        layerRoughness_9 = ba.LayerRoughness(0.39, 0.6, 30.0*nm)
+        layerRoughness_10 = ba.LayerRoughness(0.57, 0.6, 30.0*nm)
+        layerRoughness_11 = ba.LayerRoughness(0.39, 0.6, 30.0*nm)
+        layerRoughness_12 = ba.LayerRoughness(0.57, 0.6, 30.0*nm)
+        layerRoughness_13 = ba.LayerRoughness(0.54, 0.6, 30.0*nm)
+        layerRoughness_14 = ba.LayerRoughness(0.54, 0.6, 30.0*nm)
+        #print("d")
+        multiLayer_1 = ba.MultiLayer()
+        multiLayer_1.setCrossCorrLength(2000)
+        multiLayer_1.addLayer(layer_1)
+        multiLayer_1.addLayerWithTopRoughness(layer_2, layerRoughness_1)
+        multiLayer_1.addLayerWithTopRoughness(layer_3, layerRoughness_2)
+        multiLayer_1.addLayerWithTopRoughness(layer_4, layerRoughness_3)
+        multiLayer_1.addLayerWithTopRoughness(layer_5, layerRoughness_4)
+        multiLayer_1.addLayerWithTopRoughness(layer_6, layerRoughness_5)
+        multiLayer_1.addLayerWithTopRoughness(layer_7, layerRoughness_6)
+        multiLayer_1.addLayerWithTopRoughness(layer_8, layerRoughness_7)
+        multiLayer_1.addLayerWithTopRoughness(layer_9, layerRoughness_8)
+        multiLayer_1.addLayerWithTopRoughness(layer_10, layerRoughness_9)
+        multiLayer_1.addLayerWithTopRoughness(layer_11, layerRoughness_10)
+        multiLayer_1.addLayerWithTopRoughness(layer_12, layerRoughness_11)
+        multiLayer_1.addLayerWithTopRoughness(layer_13, layerRoughness_12)
+        multiLayer_1.addLayerWithTopRoughness(layer_14, layerRoughness_13)
+        multiLayer_1.addLayerWithTopRoughness(layer_15, layerRoughness_14)
+        t1_stop = process_time.default_timer()
+        #print("Sample_ " ,t1_stop-t1_start)
+        return multiLayer_1
+    
+    
+    def RunSim(_simulation):
+        import ba_plot
+        #t1_start = process_time.default_timer()
+        #_simulation.getOptions().setNumberOfBatches(10)
+        #_simulation.getOptions().setNumberOfThreads(6)  
+        #print(_simulation.numberOfSimulationElements())  
+        _simulation.getOptions().setUseAvgMaterials(True)
+        _simulation.getOptions().setIncludeSpecular(False)
+        #_simulation.getOptions().setMonteCarloIntegration(False)
+    
+        background = ba.ConstantBackground(5e+01)
+        _simulation.setBackground(background)
+        ba_plot.run_and_plot(_simulation)
+        #_simulation.runSimulation()  
+        #result = _simulation.result()   
+        #hist = result.histogram2d(ba.Axes.QSPACE)
+        #resultarr = hist.array()
+        #resultAll.append(resultarr)
+        #t1_stop = process_time.default_timer()
+        #print("Sim_ " ,t1_stop-t1_start)
+        #return result, hist, resultarr
     
     
     
-def TestNormal():
-    #print( len(resultAll))
-    t1_start = process_time.default_timer()
-    for index in range(2):
-         print(StartSim(index))
-    t1_stop = process_time.default_timer()
-    print("\n NOThreading " ,t1_stop-t1_start)
-
-
-
-
-
+    def StartSim(thrId,self):
+        #print("\n ThreadStart " ,thrId)
+        self.RunSim(self.InitSim(self.InitSample(), self.InitBeam(), self.InitDetector()))
+        #print("\n ThreadEnd " ,thrId)
+        return ("DONE " ,thrId)
+        #fig = plt.figure(figsize=(6, 3.2))
+        
+       # ax = fig.add_subplot()
+       # ax.set_title('colorMap')
+        #plt.imshow(resArr)
+        
+    
+    
+    def Test(self):
+        # print(threading.activeCount())
+        # t1_start = process_time.default_timer()
+        # with concurrent.futures.ProcessPoolExecutor() as executer:
+        #     results = [executer.submit(StartSim,x) for x in range(10)]
+        # for f in concurrent.futures.as_completed(results):
+        #     print(f.result())
+        
+        
+        
+        
+        #threads = list()
+        #main_thread = threading.currentThread()
+        # for index in range(threading.activeCount()):
+        #     x = threading.Thread(target=StartSim, args=(index,))
+        #     threads.append(x)
+        #     #x.start()
+        # for j in threads:
+        #     j.start()
+    
+        # # Ensure all of the processes have finished
+        # for j in threads:
+        #     j.join()    
+            
+        t1_stop = process_time.default_timer()
+        print("\n ThreadingT " ,t1_stop-t1_start)
+        self.TestNormal()
+        
+        
+        
+    def TestNormal(self):
+        #print( len(resultAll))
+        t1_start = process_time.default_timer()
+        for index in range(2):
+             print(self.StartSim(index))
+        t1_stop = process_time.default_timer()
+        print("\n NOThreading " ,t1_stop-t1_start)
+    
+    
+    
+    
+    
 
 
 
