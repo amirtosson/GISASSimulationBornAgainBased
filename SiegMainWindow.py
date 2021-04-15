fadf6fc96c817e92f801ea4c138820178fb2cac5
Amir Tosson <45110899+amirtosson@users.noreply.github.com>
2021-04-15 14:31:19 +0200

    APIs

--- SiegMainWindow.py
+++ SiegMainWindow.py
@@ -11,10 +11,10 @@ import SiegSimulationControls as _siegSim
 
 
 class SiegMainWindow(QtWidgets.QMainWindow):
-    g = 0
     sampleIsReady = False
     detectorIsReady = False
     simIsReady = False
+    beamIsReady = False
     rndMatrix = [0,0,0,0]
     rndParasAreOK = False
     def __init__(self, *args, **kwargs):
@@ -28,12 +28,20 @@ class SiegMainWindow(QtWidgets.QMainWindow):
         self.configSampleButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogCancelButton')))
         self.configDetectorButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogCancelButton')))
         self.configSimButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogCancelButton')))
+        self.configBeamButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogCancelButton')))
+        self.submitButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_ArrowRight')))
         self.autoRandCheckBox.stateChanged.connect(self.UseAutomaticRandomization)
         self.configSampleButton.clicked.connect(self.OpenSamplePage)
         self.configDetectorButton.clicked.connect(self.OpenDetectorPage)
+        self.configBeamButton.clicked.connect(self.OpenBeamPage)
         self.configSimButton.clicked.connect(self.OpenSimPage)
         self.randomParaOKButton.clicked.connect(self.SubmitRandomizationPara)
-        
+        self.setButton.clicked.connect(self.SetLayers)
+        self.tabWidget.currentChanged.connect(self.SubmitButtonText)
+        self.submitButton.clicked.connect(self.SumbitUserInput)
+
+
+
     def speaking_method(self):                                              
         _siegSim.Test()
         
@@ -46,17 +54,40 @@ class SiegMainWindow(QtWidgets.QMainWindow):
     def UseAutomaticRandomization(self):
         if self.autoRandCheckBox.isChecked():
             self.randGroupBox.setHidden(False)
+            self.setButton.setEnabled(False)
         else:
            self.randGroupBox.setHidden(True)
     
+    
+    def SubmitButtonText(self, ind):
+        self.submitButton.setEnabled(True)
+        if ind == 0:
+            self.submitButton.setText("Submit sample")
+        elif ind == 1:
+            self.submitButton.setText("Submit detector")
+        elif ind == 2:
+            self.submitButton.setText("Submit beam")
+        else:   
+            self.submitButton.setText("Start simulation")
+            if not self.simIsReady or not self.sampleIsReady or not self.beamIsReady or not self.detectorIsReady:
+                self.submitButton.setEnabled(False)
+                
+    
     def OpenSamplePage(self):                                              
         self.tabWidget.setCurrentIndex(0)
+        self.SubmitButtonText(0)
     
     def OpenDetectorPage(self):                                              
-        self.tabWidget.setCurrentIndex(1)    
+        self.tabWidget.setCurrentIndex(1) 
+        self.SubmitButtonText(1)
+    
+    def OpenBeamPage(self):                                              
+        self.tabWidget.setCurrentIndex(2) 
+        self.SubmitButtonText(2)
     
     def OpenSimPage(self):                                              
-        self.tabWidget.setCurrentIndex(2)
+        self.tabWidget.setCurrentIndex(3)
+        self.SubmitButtonText(3)
         
     def UpdateTODOButtonsIcons(self):
         if self.sampleIsReady:
@@ -68,6 +99,11 @@ class SiegMainWindow(QtWidgets.QMainWindow):
             self.configDetectorButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogApplyButton')))
         else:
             self.configDetectorButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogCancelButton')))
+            
+        if self.beamIsReady:      
+            self.configBeamButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogApplyButton')))
+        else:                         
+            self.configBeamButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogCancelButton')))
   
         if self.simIsReady:      
             self.configSimButton.setIcon(self.style().standardIcon(getattr(QtWidgets.QStyle, 'SP_DialogApplyButton')))
@@ -84,8 +120,45 @@ class SiegMainWindow(QtWidgets.QMainWindow):
             self.errorText.setText("Enteries are not valid")
         else:
             self.errorText.setText("")
+            self.setButton.setEnabled(True)
+            
     def CheckRndParas(self,rndArr):   
         if rndArr[1] >= rndArr[0]:
             return True
         else:
             return False
+        
+    def SetLayers(self):
+        numRows = self.numOfLayerSpinBox.value()
+        self.layerTable.setRowCount(numRows)
+        for row in range(self.numOfLayerSpinBox.value()):
+            #self.layerTable.insertRow(self.layerTable.rowCount())
+            item = QtWidgets.QTableWidgetItem(str(row+1))
+            item.setFlags(QtCore.Qt.ItemIsEnabled)
+            self.layerTable.setItem(row, 0, item)
+            self.layerTable.setItem(row, 1, QtWidgets.QTableWidgetItem("Layer_" +  str(row+1)))
+            if self.autoRandCheckBox.isChecked():
+                self.layerTable.setItem(row, 2, QtWidgets.QTableWidgetItem("RANDOM " +  str(self.rndMatrix[0]) + ":" + str(self.rndMatrix[1])))
+                self.layerTable.setItem(row, 3, QtWidgets.QTableWidgetItem("RANDOM " +  str(self.rndMatrix[2]) + ":" + str(self.rndMatrix[3])))
+            else:
+                self.layerTable.setItem(row, 2, QtWidgets.QTableWidgetItem(str(0.0)))
+                self.layerTable.setItem(row, 3, QtWidgets.QTableWidgetItem(str(0.0)))
+
+    def SumbitUserInput(self):
+       switcher = {0: self.SubmitSample(), 1: self.SubmitDetector(), 2: self.SubmitBeam(), 3: self.StartSim()} 
+       func = switcher.get(0) 
+       func
+       
+    def SubmitSample(self):
+        print("SubmitSample")
+        
+    def SubmitBeam(self):
+        print("SubmitBeam")       
+        
+    def SubmitDetector(self):
+        print("SubmitDetector")        
+        
+    def StartSim(self):
+        print("StartSim")        
+        
+        
